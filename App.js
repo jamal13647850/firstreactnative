6f9dc1f74834eff89b570547b1c0c5fbff705e00
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2021-01-03 16:59:45 +0330

    Event Handler

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
 
 export default function App() {
   const [firstName,setFirstName]=useState('جمال');
+
+  const handleNameChange = () =>{
+    setFirstName("کمال");
+  }
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.jamtext}>Hello ! Open up App.js to start working on your app!</Text>
       </View>
       <Text>این یک متن تست هست</Text>
-      <Text>سلام {firstName}</Text>
+      <View>
+        <Text>سلام {firstName}</Text>
+        <Button title="کلیک کن" onPress={handleNameChange}/>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
