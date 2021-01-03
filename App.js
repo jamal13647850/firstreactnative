47120fe1816d2da961dc76b25b8c64789091ea14
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2021-01-03 16:03:46 +0330

    Text View Styles

--- App.js
+++ App.js
@@ -5,7 +5,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello ! Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.jamtext}>Hello ! Open up App.js to start working on your app!</Text>
+      </View>
+      <Text>این یک متن تست هست</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +21,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    backgroundColor: 'purple',
+    padding: 20
+  },
+  jamtext:{
+    color: 'white',
+    fontWeight: 'bold'
+  }
 });
