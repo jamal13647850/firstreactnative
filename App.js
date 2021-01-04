25b80e2bc282d7af506a03728890e608b2e01bdd
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2021-01-04 21:29:40 +0330

    Custom fonts

--- App.js
+++ App.js
@@ -2,6 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import React,{useState} from 'react';
 import { StyleSheet, Text, View,Button,TextInput ,ScrollView, FlatList,TouchableOpacity} from 'react-native';
 import Playground from "./components/Playground"
+import {AppLoading} from 'expo';
+import * as Font from 'expo-font';
+
+
+
+const getFonts=()=>{
+  return Font.loadAsync({ 
+    //yekan:require('./assets/fonts/byekan.ttf')
+  })
+}
+
 export default function App() {
   const [firstName,setFirstName]=useState('جمال');
   const [age,setAge]=useState();
@@ -16,6 +27,8 @@ export default function App() {
     {id:8,fullname:"استاد صدفی"}
   ]);
 
+  const [fontLoading,setFontLoading]=useState(false);
+
   const deleteItem = id=>{
     //const filtered = masters.filter(master=>master.id!==id);
     //setMasters(filtered);
@@ -25,50 +38,109 @@ export default function App() {
   const handleNameChange = () =>{
     setFirstName("کمال");
   }
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.jamtext}>Hello ! Open up App.js to start working on your app!</Text>
-      </View>
-      <Text>این یک متن تست هست</Text>
-      <View>
-        <Text>سلام {firstName}</Text>
-        <Button title="کلیک کن" onPress={handleNameChange}/>
-      </View>
-      <View style={{margin:30,backgroundColor:'orange',borderRadius:10,width:300}}>
-        <Text>سن: {age}</Text>
-      </View>
-      <Text>
-        آقا چند سالته:
-      </Text>
-      <TextInput style={styles.textinput} keyboardType="numeric" onChangeText={value =>setAge(value)}/>
-      
-      <FlatList keyExtractor={item => item.id.toString()} data={masters} renderItem={({item:master})=>(
-        <TouchableOpacity onPress={()=>{deleteItem(master.id)}}>
-          <Text style={styles.card}>
-            {master.fullname} 
-          </Text>
-        </TouchableOpacity>
+  // if(fontLoading){
+  //   return (
+  //     <View style={styles.container}>
+  //       <View style={styles.header}>
+  //         <Text style={styles.jamtext}>Hello ! Open up App.js to start working on your app!</Text>
+  //       </View>
+  //       <Text style={styles.testText}>این یک متن تست هست</Text>
+  //       <Text >این یک متن تست هست</Text>
+  //       <View>
+  //         <Text>سلام {firstName}</Text>
+  //         <Button title="کلیک کن" onPress={handleNameChange}/>
+  //       </View>
+  //       <View style={{margin:30,backgroundColor:'orange',borderRadius:10,width:300}}>
+  //         <Text>سن: {age}</Text>
+  //       </View>
+  //       <Text>
+  //         آقا چند سالته:
+  //       </Text>
+  //       <TextInput style={styles.textinput} keyboardType="numeric" onChangeText={value =>setAge(value)}/>
         
-      )}/>
+  //       <FlatList keyExtractor={item => item.id.toString()} data={masters} renderItem={({item:master})=>(
+  //         <TouchableOpacity onPress={()=>{deleteItem(master.id)}}>
+  //           <Text style={styles.card}>
+  //             {master.fullname} 
+  //           </Text>
+  //         </TouchableOpacity>
+          
+  //       )}/>
+  
+  //       {/* <ScrollView>
+  //         {masters.map(master=>{
+  //           return (
+  //             <View key={master.id}>
+                
+  //               <Text style={styles.card}>
+  //                 {master.fullname} 
+  //               </Text>
+  //             </View>
+  //           )
+  //         })}
+  //       </ScrollView> */}
+        
+  //       <Playground></Playground>
+  //       <StatusBar style="auto" />
+  //     </View>
+  //   );
+  // }
+  // else{
+  //   return(
+  //     <AppLoading 
+  //       startAsync={getFonts}
+  //       onFinish={()=>setFontLoading(true)}
+  //     />
+  //   )
+  // }
 
-      {/* <ScrollView>
-        {masters.map(master=>{
-          return (
-            <View key={master.id}>
-              
-              <Text style={styles.card}>
-                {master.fullname} 
-              </Text>
-            </View>
-          )
-        })}
-      </ScrollView> */}
-      
-      <Playground></Playground>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+     return (
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <Text style={styles.jamtext}>Hello ! Open up App.js to start working on your app!</Text>
+        </View>
+        <Text >این یک متن تست هست</Text>
+        <Text >این یک متن تست هست</Text>
+        <View>
+          <Text>سلام {firstName}</Text>
+          <Button title="کلیک کن" onPress={handleNameChange}/>
+        </View>
+        <View style={{margin:30,backgroundColor:'orange',borderRadius:10,width:300}}>
+          <Text>سن: {age}</Text>
+        </View>
+        <Text>
+          آقا چند سالته:
+        </Text>
+        <TextInput style={styles.textinput} keyboardType="numeric" onChangeText={value =>setAge(value)}/>
+        
+        <FlatList keyExtractor={item => item.id.toString()} data={masters} renderItem={({item:master})=>(
+          <TouchableOpacity onPress={()=>{deleteItem(master.id)}}>
+            <Text style={styles.card}>
+              {master.fullname} 
+            </Text>
+          </TouchableOpacity>
+          
+        )}/>
+  
+        {/* <ScrollView>
+          {masters.map(master=>{
+            return (
+              <View key={master.id}>
+                
+                <Text style={styles.card}>
+                  {master.fullname} 
+                </Text>
+              </View>
+            )
+          })}
+        </ScrollView> */}
+        
+        <Playground></Playground>
+        <StatusBar style="auto" />
+      </View>
+    );
+
 }
 
 const styles = StyleSheet.create({
@@ -101,5 +173,8 @@ const styles = StyleSheet.create({
     width: 300,
     padding:5,
     margin:20,
+  },
+  testText:{
+    fontFamily:'yekan'
   }
 });
