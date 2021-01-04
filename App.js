2fc2ffdc2ccb762712e131d31dcff83b334eb032
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2021-01-04 21:57:23 +0330

    Global style

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View,Button,TextInput ,ScrollView, FlatList,Touchable
 import Playground from "./components/Playground"
 import {AppLoading} from 'expo';
 import * as Font from 'expo-font';
+import {styles} from './components/styles/globalStyle'
 
 
 
@@ -142,39 +143,3 @@ export default function App() {
     );
 
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header:{
-    backgroundColor: 'purple',
-    padding: 20
-  },
-  jamtext:{
-    color: 'white',
-    fontWeight: 'bold'
-  },
-  textinput:{
-    borderWidth:1,
-    borderColor:"lightgrey",
-    borderRadius:10,
-    padding:15,
-    margin:10,
-    width: 250,
-    textAlign:"center",
-    fontSize:15,
-  },
-  card:{
-    backgroundColor: 'green',
-    width: 300,
-    padding:5,
-    margin:20,
-  },
-  testText:{
-    fontFamily:'yekan'
-  }
-});
---
+++ components/styles/globalStyle.js
@@ -0,0 +1,39 @@
+import { StyleSheet} from 'react-native';
+
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    header:{
+      backgroundColor: 'purple',
+      padding: 20
+    },
+    jamtext:{
+      color: 'white',
+      fontWeight: 'bold'
+    },
+    textinput:{
+      borderWidth:1,
+      borderColor:"lightgrey",
+      borderRadius:10,
+      padding:15,
+      margin:10,
+      width: 250,
+      textAlign:"center",
+      fontSize:15,
+    },
+    card:{
+      backgroundColor: 'green',
+      width: 300,
+      padding:5,
+      margin:20,
+    },
+    testText:{
+      fontFamily:'yekan'
+    }
+  });
+  
