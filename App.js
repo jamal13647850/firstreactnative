2750cf676389353c474e571a924306c00ea79569
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2021-01-07 22:48:06 +0330

    Configuring Header Bar

--- App.js
+++ App.js
@@ -5,19 +5,34 @@ import Home from './screens/home'
 import About from './screens/about'
 import Details from './screens/details'
 
-const Stack=createStackNavigator();
+const Stack = createStackNavigator();
 
 
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-          <Stack.Screen name="HOME" component={Home} />
-          <Stack.Screen name="ABOUT" component={About} />
-          <Stack.Screen name="DETAILS" component={Details} 
-            initialParams={{id:'3',text:'خدانگهدار'}}
-          />
+      <Stack.Navigator
+        screenOptions={{
+          headerTitleAlign: "center",
+          headerTintColor: "red",
+          headerStyle: {
+            backgroundColor: "orange"
+          },
+          headerTitleStyle: {
+            fontWeight: "bold",
+          }
+        }}
+      >
+        <Stack.Screen name="HOME" component={Home}
+          options={{
+            title: "خانه",
+          }}
+        />
+        <Stack.Screen name="ABOUT" component={About} />
+        <Stack.Screen name="DETAILS" component={Details}
+          initialParams={{ id: '3', text: 'خدانگهدار' }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
 
