72aa1f678abff3d6a05973f2f6875fb4ae2b7b88
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2021-01-12 00:14:46 +0330

    drawer navigation

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { createStackNavigator } from '@react-navigation/stack'
 import Home from './screens/home'
 import About from './screens/about'
 import Details from './screens/details'
-import { Button,Image } from 'react-native'
+import { Button,Image,View } from 'react-native'
+import {createDrawerNavigator} from '@react-navigation/drawer'
 
 
 const Stack = createStackNavigator();
@@ -15,36 +16,79 @@ const LogoTitle=() => {
   )
 }
 
+const HomeScreen = ({navigation})=>{
+  return(
+    <View style={{flex:1,justifyContent: 'center'}}>
+      <Button title="ورود به صفحه اطلاعات" onPress={()=>navigation.navigate("InformationScreen")}/>
+      <Button title="Open Drawer" onPress={()=>navigation.openDrawer()}/>
+      <Button title="Close Drawer" onPress={()=>navigation.closeDrawer()}/>
+      <Button title="Toggle Drawer" onPress={()=>navigation.toggleDrawer()}/>
+    </View>
+  );
+
+ };
+
+const InformationScreen = ({navigation})=>{
+  return(
+    <View style={{flex:1,justifyContent: 'center'}}>
+      <Button title="ورود به صفحه خانه" onPress={()=>navigation.navigate("HomeScreen")}/>
+    
+    </View>
+  );
+
+ };
+
+const Drawer=createDrawerNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerTitleAlign: "center",
-          headerTintColor: "red",
-          headerStyle: {
-            backgroundColor: "orange"
-          },
-          headerTitleStyle: {
-            fontWeight: "bold",
-          }
-        }}
+      <Drawer.Navigator 
+      initialRouteName="HomeScreen" 
+      drawerPosition="right"
+      drawerStyle={{
+        backgroundColor:"orange",
+        width:250
+      }}
       >
-        <Stack.Screen name="HOME" component={Home}
-          options={{
-            title: "خانه",
-            headerTitle:(props)=><LogoTitle {...props}/>,
-            headerRight:()=>(
-              <Button title="test" color="green" onPress={()=>alert("salam")}></Button>
-            )
-          }}
-        />
-        <Stack.Screen name="ABOUT" component={About} />
-        <Stack.Screen name="DETAILS" component={Details}
-          initialParams={{ id: '3', text: 'خدانگهدار' }}
-        />
-      </Stack.Navigator>
+        <Drawer.Screen name="HomeScreen" component={HomeScreen}/>
+        <Drawer.Screen 
+        name="InformationScreen" 
+        component={InformationScreen}
+        options={{
+          title:"اطلاعات",
+        }}/>
+      </Drawer.Navigator>
     </NavigationContainer>
+    //  <NavigationContainer>
+    //   <Stack.Navigator
+    //     screenOptions={{
+    //       headerTitleAlign: "center",
+    //       headerTintColor: "red",
+    //       headerStyle: {
+    //         backgroundColor: "orange"
+    //       },
+    //       headerTitleStyle: {
+    //         fontWeight: "bold",
+    //       }
+    //     }}
+    //   >
+    //     <Stack.Screen name="HOME" component={Home}
+    //       options={{
+    //         title: "خانه",
+    //         headerTitle:(props)=><LogoTitle {...props}/>,
+    //         headerRight:()=>(
+    //           <Button title="test" color="green" onPress={()=>alert("salam")}></Button>
+    //         )
+    //       }}
+    //     />
+    //     <Stack.Screen name="ABOUT" component={About} />
+    //     <Stack.Screen name="DETAILS" component={Details}
+    //       initialParams={{ id: '3', text: 'خدانگهدار' }}
+    //     />
+    //   </Stack.Navigator>
+    // </NavigationContainer> 
+
+    
 
   );
 
